3c6e2a428592c776d1526c2023da0eecd47c419d
smh1988-2 <seamhurley1988@gmail.com>
2021-11-11 11:51:19 -0800

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-const init = () => {
-  
-}
+const inputForm = document.querySelector("form");
+const title = document.querySelector("h4");
+const summary = document.querySelector("p");
 
-document.addEventListener('DOMContentLoaded', init);
+inputForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  query = e.target.children[1].value;
+  fetch(`http://localhost:3000/movies/${query}`)
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+      title.textContent = data.title;
+      summary.textContent = data.summary;
+    });
+  inputForm.reset();
+});
